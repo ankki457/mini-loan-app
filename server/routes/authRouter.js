6400879af27c62cd54a5a134f5c6899f5d3d5add
Ankit sahu <130775560+ankki457@users.noreply.github.com>
2024-11-29 00:08:04 +0530

    Delete server directory

--- server/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signup, login } from "../controllers/auth.js";
-
-const authRouter = Router();
-
-authRouter.post("/signup", signup);
-authRouter.post("/login", login);
-
-export default authRouter;
