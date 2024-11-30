5d09cc60679c1e7b42a9bc3b88d4fb1b4db78c3e
Ankit sahu <130775560+ankki457@users.noreply.github.com>
2024-11-30 06:19:39 +0530

    Update loanRouter.js

--- server/routes/loanRouter.js
+++ server/routes/loanRouter.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import {
-  createLoan,
-  getLoans,
-  repayLoan,
-  updateLoan,
+    createLoan,
+    getLoans,
+    repayLoan,
+    updateLoan,
 } from "../controllers/loans.js";
 import { verifyLogin } from "../controllers/utils.js";
-
 const loanRouter = Router();
 
 loanRouter.get("/", verifyLogin, getLoans);
