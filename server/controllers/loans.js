1c2ccb867da5a201361eedbbfca6edc91409a402
Ankit sahu <130775560+ankki457@users.noreply.github.com>
2024-11-30 06:24:12 +0530

    Update loans.js

--- server/controllers/loans.js
+++ server/controllers/loans.js
@@ -1,19 +1,43 @@
 import mongoose from "mongoose";
 
-const repaymentSchema = new mongoose.Schema({
-  date: { type: Date, required: true },
-  amount: { type: Number, required: true },
-  status: { type: String, default: "pending" },
-});
-
-const loanSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  amount: { type: Number, required: true },
-  terms: { type: Number, required: true },
-  repayments: [repaymentSchema],
-  remainingAmount: { type: Number, required: true },
-  status: { type: String, default: "pending" },
-});
+const loanSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      require: true,
+    },
+    terms: {
+      type: Number,
+      require: true,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "Auth",
+    },
+    status: {
+      type: String,
+      default: "pending",
+    },
+    outStanding:{
+      type:Number
+    },
+    remainingAmount:{
+      type:Number
+    },
+    repayments: [
+      {
+        date: String,
+        amount: Number,
+        status:{
+          type:String,
+          default:"pending"
+        }
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const Loan = mongoose.model("Loan", loanSchema);
 
